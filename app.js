7ad3d5cb732e94719033f26adab35b81a0d90e9d
Jordan Benner <jordanbenner@outlook.com>
2017-06-09 09:54:11 -0500

    Public chat

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('view engine', 'hbs');
+
+app.get('/', function (request, response){
+response.render('chat.hbs');
+});
+
+io.on('connection', function (client){
+  console.log('Connected');
+
+  client.on('disconnect', function (){
+    console.log(' EXITED')
+  });
+  client.on('incoming', function (msg){
+    io.emit('chat-msg', msg)
+  })
+});
+http.listen(8000,function (){
+console.log('Listening on port 8000')
+});
+
+
+//private chat//
